dedd7eaafa6666816f38a23d95ab416b5ce26a19
harshvardhan21222 <harshvardhan@topahead.com>
2024-04-25 13:32:13 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,7 +1,19 @@
 function longestSubstring(s) {
-    // Implementation of longestSubstring function
-}
-
-module.exports = { longestSubstring };
-
-
+    let maxLength = 0;
+    let start = 0;
+    const charMap = {};
+  
+    for (let i = 0; i < s.length; i++) {
+      const currentChar = s[i];
+      if (charMap[currentChar] >= start) {
+        start = charMap[currentChar] + 1;
+      }
+      charMap[currentChar] = i;
+      maxLength = Math.max(maxLength, i - start + 1);
+    }
+  
+    return maxLength;
+  }
+  
+  module.exports = { longestSubstring };
+  
